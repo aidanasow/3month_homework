153e6464ed6c247700ae8e672a9e766470734e25
aidanasow <138320839+aidanasow@users.noreply.github.com>
2023-12-19 21:21:57 +0600

    Update lesson.js добаивла дз3

--- project_program/js/lesson.js
+++ project_program/js/lesson.js
@@ -15,3 +15,48 @@ phoneButton.onclick=()=>{
         phoneInput.value=' '
     }
 }
+
+//TAB SLIDER
+const tabContentBlocks=document.querySelectorAll('.tab_content_block')
+const tabs=document.querySelectorAll('.tab_content_item')
+const tabsParent=document.querySelector('.tab_content_items')
+let currentTab=0;
+
+const hideTabContent=()=>{
+    tabContentBlocks.forEach(tabContentBlock=>{
+        tabContentBlock.style.display='none';
+    })
+    tabs.forEach(tab=>{
+        tab.classList.remove('tab_content_item_active')
+    })
+}
+const showTabContent=(tabIndex=0)=>{
+    tabContentBlocks[tabIndex].style.display='block'
+    tabs[tabIndex].classList.add('tab_content_item_active')
+}
+
+const automaticSlider=(tabIndex)=>{
+    hideTabContent()
+    currentTab=(currentTab+1)%tabs.length
+    showTabContent(currentTab)
+}
+
+
+hideTabContent()
+showTabContent()
+setInterval(automaticSlider, 4000)
+
+tabsParent.onclick = (event)=>{
+    if (event.target.classList.contains('tab_content_item')){
+        tabs.forEach((tab, tabIndex)=>{
+            if (event.target===tab){
+                hideTabContent()
+                currentTab=tabIndex
+                showTabContent(currentTab)
+            }
+        })
+    }
+}
+
+
+
