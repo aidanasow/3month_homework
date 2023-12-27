ccfe7af77118931698ebd9171ffb23fcf14f2591
aidanasow <138320839+aidanasow@users.noreply.github.com>
2023-12-27 10:52:52 +0600

    Update lesson.js add hw6

--- project_program/js/lesson.js
+++ project_program/js/lesson.js
@@ -94,4 +94,43 @@ convertor(som, usd,euro, 'som')
 convertor(usd, som,euro, 'usd')
 convertor(euro, usd,som, 'euro')
 
+//CARD SWITCHER
+const card=document.querySelector('.card'),
+    btnNext=document.querySelector('#btn-next'),
+    btnPrev=document.querySelector('#btn-prev')
+let count=1
+const doFetch=()=>{
+    fetch(`https://jsonplaceholder.typicode.com/todos/${count}`)
+        .then(response=>response.json())
+        .then(data=>{
+            card.innerHTML=`
+            <p>${data.title}</p>
+            <p style="color: ${data.completed ? 'green':'red'} ">${data.completed}</p>
+            <span>${data.id}</span>
+            `
+        })
+}
+doFetch()
+btnNext.addEventListener('click',()=>{
+    count++
+    if (count>200){
+        count=1
+    }
+    doFetch()
+})
+
+btnPrev.addEventListener('click',()=>{
+    count--
+    if (count<1){
+        count=200
+    }
+    doFetch()
+})
+
+//2 пунк дз fetch request
+
+fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response=>response.json())
+    .then(data=>console.log(data))
+
 
