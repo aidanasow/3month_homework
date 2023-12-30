d153f5649dd7136e7e32fac141bbdb251968f8ed
aidanasow <138320839+aidanasow@users.noreply.github.com>
2023-12-30 11:47:17 +0600

    Update lesson.js

--- project_program/js/lesson.js
+++ project_program/js/lesson.js
@@ -1,57 +1,55 @@
 //PHONE CHECKER lesson1
-const  phoneInput=document.getElementById('phone_input')
-const  phoneButton=document.getElementById('phone_button')
-const  phoneResult=document.getElementById('phone_result')
+const phoneInput = document.getElementById('phone_input')
+const phoneButton = document.getElementById('phone_button')
+const phoneResult = document.getElementById('phone_result')
 
-const regExp=/^\+996 ([2579]\d{2})|312 \d{2}-\d{2}-\d{2}$/
-phoneButton.onclick=()=>{
-    if (regExp.test(phoneInput.value)){
-        phoneResult.innerHTML='OK'
-        phoneResult.style.color='green'
-        phoneInput.value=' '
-    } else{
-        phoneResult.innerHTML='incorrect value'
-        phoneResult.style.color='red'
-        phoneInput.value=' '
+const regExp = /^\+996 ([2579]\d{2})|312 \d{2}-\d{2}-\d{2}$/
+phoneButton.onclick = () => {
+    if (regExp.test(phoneInput.value)) {
+        phoneResult.innerHTML = 'OK'
+        phoneResult.style.color = 'green'
+        phoneInput.value = ' '
+    } else {
+        phoneResult.innerHTML = 'incorrect value'
+        phoneResult.style.color = 'red'
+        phoneInput.value = ' '
     }
 }
 
 //TAB SLIDER
-const tabContentBlocks=document.querySelectorAll('.tab_content_block')
-const tabs=document.querySelectorAll('.tab_content_item')
-const tabsParent=document.querySelector('.tab_content_items')
-let currentTab=0;
-
-const hideTabContent=()=>{
-    tabContentBlocks.forEach(tabContentBlock=>{
-        tabContentBlock.style.display='none';
+const tabContentBlocks = document.querySelectorAll('.tab_content_block')
+const tabs = document.querySelectorAll('.tab_content_item')
+const tabsParent = document.querySelector('.tab_content_items')
+const hideTabContent = () => {
+    tabContentBlocks.forEach(tabContentBlock => {
+        tabContentBlock.style.display = 'none'
     })
-    tabs.forEach(tab=>{
+    tabs.forEach(tab => {
         tab.classList.remove('tab_content_item_active')
     })
 }
-const showTabContent=(tabIndex=0)=>{
-    tabContentBlocks[tabIndex].style.display='block'
+
+const showTabContent = (tabIndex = 0) => {
+    tabContentBlocks[tabIndex].style.display = 'block'
     tabs[tabIndex].classList.add('tab_content_item_active')
 }
 
-const automaticSlider=(tabIndex)=>{
+let currentTab = 0;
+const automaticSlider = (tabIndex) => {
     hideTabContent()
-    currentTab=(currentTab+1)%tabs.length
+    currentTab = (currentTab + 1) % tabs.length
     showTabContent(currentTab)
 }
-
-
 hideTabContent()
 showTabContent()
 setInterval(automaticSlider, 4000)
 
-tabsParent.onclick = (event)=>{
-    if (event.target.classList.contains('tab_content_item')){
-        tabs.forEach((tab, tabIndex)=>{
-            if (event.target===tab){
+tabsParent.onclick = (event) => {
+    if (event.target.classList.contains('tab_content_item')) {
+        tabs.forEach((tab, tabIndex) => {
+            if (event.target === tab) {
                 hideTabContent()
-                currentTab=tabIndex
+                currentTab = tabIndex
                 showTabContent(currentTab)
             }
         })
@@ -59,78 +57,104 @@ tabsParent.onclick = (event)=>{
 }
 
 //convertor
-const som=document.querySelector('#som')
-const usd=document.querySelector('#usd')
-const euro=document.querySelector('#eur')
+const som = document.querySelector('#som')
+const usd = document.querySelector('#usd')
+const euro = document.querySelector('#eur')
 
-const convertor=(element, targetElement,targetElement2, current)=>{
-    element.oninput=()=>{
-        const xhr=new XMLHttpRequest()
-        xhr.open('GET','../data/converter.json')
-        xhr.setRequestHeader('Content-type', 'application/json')
-        xhr.send()
-        xhr.onload=()=>{
-            const data=JSON.parse(xhr.response)
+const converter = async (element, targetElement, targetElement2, current) => {
+    element.oninput = async () => {
+        try {
+            const response = await fetch('../data/converter.json')
+            const data = await response.json()
             switch (current) {
                 case 'som':
-                    targetElement.value=(element.value/data.usd).toFixed(2)
-                    targetElement2.value=(element.value/data.euro).toFixed(2)
-                    break
+                    targetElement.value = (element.value / data.usd).toFixed(2)
+                    targetElement2.value = (element.value / data.euro).toFixed(2)
+                    break;
                 case 'usd':
-                    targetElement.value=(element.value*data.usd).toFixed(2)
-                    targetElement2.value=(targetElement.value/data.euro).toFixed(2)
-                    break
+                    targetElement.value = (element.value / data.euro * data.usd).toFixed(2)
+                    targetElement2.value = (element.value * data.usd).toFixed(2)
+                    break;
                 case 'euro':
-                    targetElement.value=(element.value/data.usd*data.euro).toFixed(2)
-                    targetElement2.value=(element.value*data.euro).toFixed(2)
-                    break
+                    targetElement.value = (element.value * data.euro).toFixed(2)
+                    targetElement2.value = (element.value / data.usd * data.euro).toFixed(2)
+                    break;
                 default:
                     break
             }
+        } catch (e) {
+            console.log(e.message)
         }
     }
+
 }
-convertor(som, usd,euro, 'som')
-convertor(usd, som,euro, 'usd')
-convertor(euro, usd,som, 'euro')
+
+converter(som, usd, euro, 'som')
+converter(usd, euro, som, 'usd')
+converter(euro, som, usd, 'euro')
 
 //CARD SWITCHER
-const card=document.querySelector('.card'),
-    btnNext=document.querySelector('#btn-next'),
-    btnPrev=document.querySelector('#btn-prev')
-let count=1
-const doFetch=()=>{
-    fetch(`https://jsonplaceholder.typicode.com/todos/${count}`)
-        .then(response=>response.json())
-        .then(data=>{
-            card.innerHTML=`
-            <p>${data.title}</p>
-            <p style="color: ${data.completed ? 'green':'red'} ">${data.completed}</p>
+const card = document.querySelector('.card'),
+    btnNext = document.querySelector('#btn-next'),
+    btnPrev = document.querySelector('#btn-prev')
+let count = 1
+const doFetch = async () => {
+    try{
+        const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${count}`)
+        const data = await response.json()
+        card.innerHTML = `
+            <p ">${data.title}</p>
+            <p style="color: ${data.completed ? 'green' : 'red'} ">${data.completed}</p>
             <span>${data.id}</span>
             `
-        })
+    }catch (e){
+        console.log(e.message)
+    }
 }
 doFetch()
-btnNext.addEventListener('click',()=>{
+
+btnNext.addEventListener('click', () => {
     count++
-    if (count>200){
-        count=1
+    if (count > 200) {
+        count = 1
     }
     doFetch()
 })
 
-btnPrev.addEventListener('click',()=>{
+btnPrev.addEventListener('click', () => {
     count--
-    if (count<1){
-        count=200
+    if (count < 1) {
+        count = 200
     }
     doFetch()
 })
 
 //2 пунк дз fetch request
 
-fetch('https://jsonplaceholder.typicode.com/posts')
-    .then(response=>response.json())
-    .then(data=>console.log(data))
+const doRequest = async () => {
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+        const data = await response.json()
+        console.log(data)
+    } catch (e) {
+        console.log(e.message)
+    }
+}
+doRequest()
+//WEATHER
+const cityNameInput = document.querySelector('.cityName'),
+    city = document.querySelector('.city'),
+    temp = document.querySelector('.temp')
 
-
+const BASE_URL = 'http://api.openweathermap.org'
+const API_KEY = 'e417df62e04d3b1b111abeab19cea714'
+cityNameInput.addEventListener('input', async (event) => {
+    try {
+        const response = await fetch(`${BASE_URL}/data/2.5/weather?q=${event.target.value}&appid=${API_KEY}`)
+        const data = await response.json()
+        city.innerHTML = data.name ? data.name : 'Город не найден...'
+        temp.innerHTML = data?.main?.temp ? Math.round(data.main.temp - 273.15) + '&deg; C' : '...'
+    } catch (e) {
+        console.log(e.message)
+    }
+})
